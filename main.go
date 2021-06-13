02951b1640c88ae2e6cade5956f1d1dfc682bffa
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-06-12 22:19:49 -0700

    Forgot one

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 )
 
 func main() {
-	for a := 1; a < len(os.Args); a++ {
-		if os.Args[1] == "--help" || os.Args[1] == "-h" {
-			println("Welcome to the Brewery!")
-			println("")
-		}
-	}
+	println("Hello Go!")
 }
 
 func println(message string) {
