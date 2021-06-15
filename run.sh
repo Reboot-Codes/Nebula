32d23f282f3ca74dedda8cab2e609ffd158fa7aa
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-06-15 14:06:36 -0700

    Updated run.sh to use Mage

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
-# Build the latest version of the project
-go build
-# Replace the executable with the new one
-cd ./bin
-rm ./nebula
-cd ..
-mv nebula bin
+# Build and Update to the latest build
+mage update
 # Run the executable, passing in any arguments
-./bin/nebula $*
+nebula $*
