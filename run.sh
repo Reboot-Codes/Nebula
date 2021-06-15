707a8301c87775da4732b8c0c33c88b14a7243f2
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-06-15 10:42:41 -0700

    Put the executable in a ./bin folder

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 # Build the latest version of the project
 go build
+# Replace the executable with the new one
+cd ./bin
+rm ./nebula
+cd ..
+mv nebula bin
 # Run the executable, passing in any arguments
-./nebula $*
+./bin/nebula $*
