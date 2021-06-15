4e407d43bcc42e16ac59a7f7cb3f17fb4b7a0fec
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-06-15 13:07:06 -0700

    Added Magefile

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/fatih/color v1.12.0
+	github.com/magefile/mage v1.11.0 // indirect
 	github.com/urfave/cli v1.22.5
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
---
+++ magefile.go
@@ -0,0 +1,65 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/fatih/color"
+	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+)
+
+// Default target to run when none is specified
+// If not set, running mage will list available targets
+var Default = Update
+
+func printStatus(statusColor color.Attribute, statusMessage string) {
+
+	color.New(statusColor).Printf("==>")
+	fmt.Println(" " + statusMessage)
+
+}
+
+// A build step that requires additional params, or platform specific steps for example
+func Build() error {
+
+	//mg.Deps(InstallDeps)
+	printStatus(color.FgHiBlue, "Building...")
+	cmd := exec.Command("go", "build", "-o", "nebula", ".")
+	return cmd.Run()
+
+}
+
+// A custom install step if you need your bin someplace other than go/bin
+func Install() error {
+	mg.Deps(Build)
+	printStatus(color.FgHiBlue, "Installing...")
+	return os.Rename("nebula", "/Users/reboot/go/bin/nebula")
+}
+
+// Update the binary
+func Update() {
+	mg.Deps(Uninstall)
+	mg.Deps(Install)
+}
+
+// Custom uninstall script
+func Uninstall() error {
+	printStatus(color.FgHiBlue, "Uninstalling...")
+	return os.RemoveAll("/Users/reboot/go/bin/nebula")
+}
+
+// Manage your deps, or running package managers.
+func InstallDeps() error {
+	printStatus(color.FgHiBlue, "Installing Deps...")
+	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
+	return cmd.Run()
+}
+
+// Clean up after yourself
+func Clean() {
+	printStatus(color.FgHiBlue, "Cleaning...")
+	os.RemoveAll("nebula")
+}
